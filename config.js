eea912442e2af8e09cd84129e6f10f86a39c67eb
Vadim Dubovitsky <vadim.dubovitsky@gmail.com>
2019-12-11 17:33:21 +0300

    Disable search

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const config = {
 			{ "text": "", "link": ""}
 		],
 		"search": {
-			"enabled": true,
+			"enabled": false,
 			"indexName": "learn-hasura-test",
 			"algoliaAppId": process.env.GATSBY_ALGOLIA_APP_ID,
 			"algoliaSearchKey": process.env.GATSBY_ALGOLIA_SEARCH_KEY,
